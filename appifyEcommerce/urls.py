4d8eddf24660ac0a2e05ef113dc3452bf0cbf366
mahede182 <mahede.object@gmail.com>
2026-02-12 14:56:10 +0600

    fix: deploy error

--- appifyEcommerce/urls.py
+++ appifyEcommerce/urls.py
@@ -24,7 +24,7 @@ router.register(r'order-items', OrderItemViewSet)
 urlpatterns = [
     # API Documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     
     # Authentication
